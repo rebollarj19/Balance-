f01edd8f125e65caa3a596d58fd922a56b9a17c9
rebollarj19 <88381122+rebollarj19@users.noreply.github.com>
2022-04-03 20:55:28 -0400

    Initial commit

---
+++ Assets/Course Library/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject coinPrefab;
+    private float spawnRange = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(coinPrefab, GenerateSpawnPosition(), coinPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        return new Vector3(spawnPosX, (float)-.5, spawnPosZ);
+
+    }
+}
